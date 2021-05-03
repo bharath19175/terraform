9f69059ba3eca10371ff293e5edb9a4dda544416
bharath19175 <bharath.sudharshanam@gmail.com>
2021-05-03 12:34:35 -0400

    updated files

--- provisioning/instance.tf
+++ provisioning/instance.tf
@@ -1,25 +1,29 @@
 resource "aws_key_pair" "mykey" {
-  key_name = "mykey"
+  key_name   = "mykey"
   public_key = file(var.PATH_TO_PUBLIC_KEY)
-
 }
 
 resource "aws_instance" "provisioning" {
-  instance_type = "t2micro"
-  ami = var.AMIS[var.AWS_REGION]
-  key_name = aws_key_pair.mykey.key_name
+  instance_type = "t2.micro"
+  ami           = var.AMIS[var.AWS_REGION]
+  key_name      = aws_key_pair.mykey.key_name
+  tags = {
+    Name = "nginx"
+  }
 
   provisioner "file" {
-    source = "script.sh"
+    source      = "script.sh"
     destination = "/tmp/script.sh"
   }
-  provisioner "remote_exec" {
+
+  provisioner "remote-exec" {
     inline = [
-      "chmod +x /temp/script.sh"
+      "chmod +x /tmp/script.sh",
       "sudo sed -i -e 's/\r$//' /tmp/script.sh",  # Remove the spurious CR characters.
       "sudo /tmp/script.sh",
     ]
   }
+
   connection {
     host        = coalesce(self.public_ip, self.private_ip)
     type        = "ssh"
